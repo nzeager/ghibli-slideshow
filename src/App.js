646ac2f90debcade690adfe2218626acff1c714e
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-12 19:42:45 -0400

    Added button functionality and display film info without styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import './App.css'
+import {useState} from 'react'
 import { filmData } from './film-data'
 
 function App() {
+  const [filmIndex, setFilmIndex] = useState(0)
+
+  // length of filmData
+  const len = filmData.length
+
   return (
     <div className="slides-app">
-      <h1>Your Slideshow</h1>
+      <FilmDetail filmObj={filmData[filmIndex]} filmIndex={filmIndex} setFilmIndex={setFilmIndex} len={len} />
     </div>
   )
 }
+
+const FilmDetail = ({ filmObj, filmIndex, setFilmIndex, len }) => {
+  return (
+    <>
+      <div> {filmObj.title } </div>
+      <img src={filmObj.image } alt={filmObj.title}></img>
+      <div> {filmObj.original_title } </div>
+      <div> {filmObj.release_date } </div>
+      <div> {filmObj.description } </div>
+      <button
+        onClick={() => {
+          setFilmIndex(0)
+        }}
+      > Start Over </button>
+      <button
+        onClick={() => {
+          setFilmIndex(filmIndex-1)
+        
+        }}
+        disabled={filmIndex===0}
+      > Back </button>
+      <button
+        onClick={() => {
+          setFilmIndex(filmIndex+1)
+        }}
+        disabled={filmIndex===len-1}
+      > Next </button>
+    </>
+  )
+}
+
 
 export default App
