ce083f223194cadc5c5e416536c4aa04f8396137
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-12 20:08:20 -0400

    Added styling, disabled Start Over button on first slide

--- src/App.js
+++ src/App.js
@@ -18,29 +18,36 @@ function App() {
 const FilmDetail = ({ filmObj, filmIndex, setFilmIndex, len }) => {
   return (
     <>
-      <div> {filmObj.title } </div>
-      <img src={filmObj.image } alt={filmObj.title}></img>
-      <div> {filmObj.original_title } </div>
-      <div> {filmObj.release_date } </div>
-      <div> {filmObj.description } </div>
-      <button
-        onClick={() => {
-          setFilmIndex(0)
-        }}
-      > Start Over </button>
-      <button
-        onClick={() => {
-          setFilmIndex(filmIndex-1)
-        
-        }}
-        disabled={filmIndex===0}
-      > Back </button>
-      <button
-        onClick={() => {
-          setFilmIndex(filmIndex+1)
-        }}
-        disabled={filmIndex===len-1}
-      > Next </button>
+      <div className='title'> {filmObj.title } </div>
+      <div className='info'>
+        <div className='image-info'><img src={filmObj.image } alt={filmObj.title}></img></div>
+        <div className='written-info'>
+          <div className='original-title'> {filmObj.original_title } </div>
+          <div className='release'> {filmObj.release_date } </div>
+          <div className='description'> {filmObj.description } </div>
+        </div>
+      </div>
+      <div className='buttons'>
+        <button
+          onClick={() => {
+            setFilmIndex(0)
+          }}
+          disabled={filmIndex===0}
+        > Start Over </button>
+        <button
+          onClick={() => {
+            setFilmIndex(filmIndex-1)
+          
+          }}
+          disabled={filmIndex===0}
+        > Back </button>
+        <button
+          onClick={() => {
+            setFilmIndex(filmIndex+1)
+          }}
+          disabled={filmIndex===len-1}
+        > Next </button>
+      </div>
     </>
   )
 }
